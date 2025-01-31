8b4f550708ef10dd9df7020237a558fbe0c803a1
Freezingle <bct078011@student.ioepc.edu.np>
2025-01-31 22:38:36 +0545

    firstcommit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,37 +1,63 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useNavigate } from 'react-router-dom';
+import {auth} from './firebase'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+
 import { faFacebookF, faGooglePlusG, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 import "./styles/login.css";
 function Login() {
     const [isSignIn, setIsSignIn] = useState(true);
+    const [name,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+
 
     const toggleForm = () => {
       setIsSignIn(!isSignIn);
     };
-  
+
+    const handleSignUp = async (e) => {
+      e.preventDefault();
+      setLoading(true);
+      try {
+        await createUserWithEmailAndPassword(auth, email, password);
+        navigate('/home');
+      } catch (error) {
+        alert(error.message);
+      }
+      setLoading(false);
+    };
+
+    const handleSignIn = async (e) => {
+      e.preventDefault();
+      setLoading(true);
+      try {
+        await signInWithEmailAndPassword(auth, email, password);
+        navigate('/home');
+        
+      } catch (error) {
+        alert(error.message);
+      }
+      setLoading(false);
+    };
+
     return (
       <div>
         <h2>Login to our page </h2>
         <div className={`container ${isSignIn ? "" : "right-panel-active"}`} id="container">
           {/* Sign Up Form */}
           <div className="form-container sign-up-container">
-            <form action="#">
+            <form onSubmit={handleSignUp}>
               <h1>Create Account</h1>
-              <div className="social-container">
-                <a href="#" className="social">
-                  <FontAwesomeIcon icon={faFacebookF} />
-                </a>
-                <a href="#" className="social">
-                  <FontAwesomeIcon icon={faGooglePlusG} />
-                </a>
-                <a href="#" className="social">
-                  <FontAwesomeIcon icon={faLinkedinIn} />
-                </a>
-              </div>
-              <span>or use your email for registration</span>
-              <input type="text" placeholder="Name" />
-              <input type="email" placeholder="Email" />
-              <input type="password" placeholder="Password" />
+              
+              <span>Use your email for registration</span>
+              <input type="text" placeholder="Name" value  =  {name} onChange= {(e)=>{setName(e.target.value)}} />
+              <input type="email" placeholder="Email" value={email} onChange = {(e)=>{setEmail(e.target.value)}} />
+              <input type="password" placeholder="Password" value={password} onChange = {(e)=>{setPassword(e.target.value)}} />
               <button>Sign Up</button>
               <p class="mobile">Already have an id? <a onClick = {toggleForm}> Sign In </a></p>
 
@@ -40,23 +66,13 @@ function Login() {
   
           {/* Sign In Form */}
           <div className="form-container sign-in-container">
-            <form action="#">
+            <form onSubmit = {handleSignIn}>
               <h1>Sign in</h1>
-              <div className="social-container">
-                <a href="#" className="social">
-                  <FontAwesomeIcon icon={faFacebookF} />
-                </a>
-                <a href="#" className="social">
-                  <FontAwesomeIcon icon={faGooglePlusG} />
-                </a>
-                <a href="#" className="social">
-                  <FontAwesomeIcon icon={faLinkedinIn} />
-                </a>
-              </div>
-              <span>or use your account</span>
-              <input type="email" placeholder="Email" />
-              <input type="password" placeholder="Password" />
-              <a href="#">Forgot your password?</a>
+              
+              <span>Use your account</span>
+              <input type="email" placeholder="Email" value = {email}  onChange= {(e)=>{setEmail(e.target.value)}} />
+              <input type="password" placeholder="Password" value = {password} onChange = {(e)=>{setPassword(e.target.value)}} />
+             
                 <button>Sign In</button>
                 <p class= "mobile">Donot have an Id? <a onClick={toggleForm}>Sign Up</a></p>
             </form>
